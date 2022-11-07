b443008d690af3d7dbbd63f47448bc9ca162f13e
Mikhail-Kavzov <kavzovm@mail.ru>
2022-11-07 16:43:01 +0300

    Add: Student and Client entities

---
+++ src/main/java/by/bsuir/lab03/server/main/Main.java
@@ -0,0 +1,14 @@
+package by.bsuir.lab03.server.main;
+
+import by.bsuir.lab03.server.service.ServerLogic;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ServerLogic serverLogic = new ServerLogic();
+		serverLogic.startConnection();
+	}
+
+}
